141880e7f8dc1f9b6f31638a444b34bb3aefe3ef
Asmaa-25112003 <asmaa230101389@sut.edu.eg>
2025-05-28 22:16:20 +0300

    done

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+/**
+ * PostCSS Configuration
+ * This file configures the PostCSS plugins used in the project
+ * @see https://postcss.org/
+ */
+
 export default {
     plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+        // Process Tailwind CSS directives and utilities
+        tailwindcss: {
+            // Configuration is loaded from tailwind.config.js
+        },
+        
+        // Add vendor prefixes to CSS rules
+        autoprefixer: {
+            // Supported browsers are configured in package.json browserslist
+        },
     },
 };
