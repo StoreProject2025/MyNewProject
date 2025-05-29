c6457a0c1b503e9d17589b2e76683ff0ba57b27d
fatma youssef <fatma230103456@sut.edu.eg>
2025-05-29 14:11:07 +0300

    fatma's commit

--- API project/routes/api.php
+++ API project/routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\API\AdminOrderController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
- 
+  
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
